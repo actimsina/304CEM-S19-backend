7b98ae523703363ef803fd26af99c7d65be3d2d5
Achyut Timsina <actimsina@gmail.com>
2019-11-25 10:05:08 +0545

    Basic restful routes created

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
-
 const app = express();
-
-app.get('/', function (req, res) {
-    // console.log(req.headers);
-    res.send("Hello, Class");
+app.get('/tasks', function (req, res) {
+    res.send("Send all tasks");
 });
-
+app.post('/tasks', (req, res) => {
+    res.send("Create a task");
+});
+app.put('/tasks', (req, res) => {
+    res.send("Not supported");
+});
+app.delete('/tasks', (req, res) => {
+    res.send("Delete all tasks");
+});
+app.get('/tasks/:id', (req, res) => {
+    res.send(`Send a task with ${req.params.id}`);
+});
+app.post('/tasks/:id', (req, res) => {
+    res.send("Not Supported");
+});
+app.put('/tasks/:id', (req, res) => {
+    res.send(`Update a task with id: ${req.params.id}`);
+});
+app.delete('/tasks/:id', (req, res) => {
+    res.send(`Delete a task with id: ${req.params.id}`);
+});
 app.listen(3000, () => {
     console.log('App is running at localhost:3000');
 });
