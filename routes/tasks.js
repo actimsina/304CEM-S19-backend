6a015341a0ad3bf7afab960014dc08881c3b7efc
Achyut Timsina <actimsina@gmail.com>
2019-12-05 09:57:48 +0545

    notes with all routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -44,13 +44,53 @@ router.route('/:id')
     });
 // HW for this thursday
 router.route('/:id/notes')
-    .get()
-    .post()
-    .delete();
+    .get((req, res, next) => {
+        Task.findById(req.params.id)
+            .then((task) => {
+                res.json(task.notes);
+            }).catch(next);
+    })
+    .post((req, res, next) => {
+        Task.findById(req.params.id)
+            .then((task) => {
+                task.notes.push(req.body);
+                task.save()
+                    .then((task) => {
+                        res.json(task);
+                    })
+            }).catch(next);
+    })
+    .delete((req, res, next) => {
+        Task.findById(req.params.id)
+            .then((task) => {
+                task.notes = [];
+                task.save()
+                    .then((task) => {
+                        res.json(task);
+                    })
+            }).catch(next);
+    });
 
 router.route('/:id/notes/:noteId')
-    .get()
-    .put()
+    .get((req, res, next) => {
+        Task.findById(req.params.id)
+            .then((task) => {
+                let note = task.notes.id(req.params.noteId);
+                res.json(note);
+            }).catch(next);
+    })
+    .put((req, res, next) => {
+        Task.findById(req.params.id)
+            .then((task) => {
+                let note = task.notes.id(req.params.noteId);
+                note.desc = req.body.desc;
+                task.save()
+                    .then((task) => {
+                        res.json(task.notes);
+                    })
+            }).catch(next);
+    })
+    // HW for tomorrow
     .delete();
 
 module.exports = router;
