05f5e64cf0c46c385381a7353772b33a80133257
Achyut Timsina <actimsina@gmail.com>
2019-12-19 09:54:30 +0545

    jwt with signup and login

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const taskRouter = require('./routes/tasks');
 const categoryRouter = require('./routes/categories');
 const auth = require('./routes/auth');
 const userRouter = require('./routes/users');
+const dotenv = require('dotenv').config();
 
-mongoose.connect('mongodb://127.0.0.1/demotm', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then((db) => {
         console.log("Successfully connected mongodb server");
     });
@@ -25,7 +26,7 @@ app.use('/tasks', taskRouter);
 
 // Custom Error Handling
 
-app.listen(3000, () => {
-    console.log('App is running at localhost:3000');
+app.listen(process.env.PORT, () => {
+    console.log(`App is running at localhost:${process.env.PORT}`);
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const User = require('../models/users');
 
@@ -20,14 +21,31 @@ router.post('/signup', (req, res, next) => {
                     username: req.body.username,
                     password: hash
                 }).then((user) => {
-                    res.json({ status: "Signup Success!", userId: user._id });
+                    let token = jwt.sign({ userId: user._id }, process.env.SECRET);
+                    res.json({ status: "Signup Success!", token: token });
                 }).catch(next);
             });
         });
 });
 
 router.post('/login', (req, res, next) => {
-    res.send('this is login');
+    User.findOne({ username: req.body.username })
+        .then((user) => {
+            if (user === null) {
+                let err = new Error('User not found!');
+                err.status = 401;
+                return next(err);
+            }
+            bcrypt.compare(req.body.password, user.password, function (err, status) {
+                if (!status) {
+                    let err = new Error('Password does not match!');
+                    err.status = 401;
+                    return next(err);
+                }
+                let token = jwt.sign({ userId: user._id }, process.env.SECRET);
+                res.json({ status: 'Login Successful!', token: token });
+            });
+        }).catch(next);
 });
 
 module.exports = router;
